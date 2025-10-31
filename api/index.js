d81963accdd331ce112bc6c1f3fea06433a70b40
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2025-10-31 21:02:52 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+// mongodb+srv://kumarvishal22665_db_user:C5wuGQ3tE6biJ8gp@cluster0.c6jfzgw.mongodb.net/?appName=Cluster0
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 const app = express();
 
